1d429a11551f256347176de21f9f35b29743eed5
Stephen Brennan <smb196@case.edu>
2015-07-23 16:13:03 -0400

    Add a name plugin for cbot to introduce itself.

---
+++ plugin/name.c
@@ -0,0 +1,29 @@
+/***************************************************************************//**
+
+  @file         name.c
+
+  @author       Stephen Brennan
+
+  @date         Created Thursday, 23 July 2015
+
+  @brief        CBot plugin to respond to questions about cbot.
+
+  @copyright    Copyright (c) 2015, Stephen Brennan.  Released under the Revised
+                BSD License.  See LICENSE.txt for details.
+
+*******************************************************************************/
+
+#include "cbot/cbot.h"
+
+static cbot_send_t send;
+
+static void name(cbot_t *bot, const char *channel, const char *user, const char *message)
+{
+  send(bot, channel, "My name is CBot.  My source lives at https://github.com/brenns10/cbot\n");
+}
+
+void name_load(cbot_t *bot, cbot_register_t hear, cbot_register_t respond, cbot_send_t send_)
+{
+  send = send_;
+  hear(bot, "([wW]ho|[wW]hat) +[iI]s +[cC][bB]ot\\??", name);
+}
