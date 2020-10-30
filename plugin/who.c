39c12ede1d95a6966df245ef1aa3fb71911055fe
Stephen Brennan <stephen@brennan.io>
2020-10-30 00:58:50 -0700

    plugins: add who for testing user database

---
+++ plugin/who.c
@@ -0,0 +1,45 @@
+/**
+ * who.c: CBot plugin which lists members of this channel
+ *
+ * Sample usage:
+ *
+ *     user> hi cbot
+ *     cbot> hello, user!
+ */
+
+#include <sc-collections.h>
+
+#include "cbot/cbot.h"
+
+static void who(struct cbot_message_event *event, void *user)
+{
+	struct sc_list_head list;
+	struct cbot_user_info *ui;
+	struct sc_charbuf buf;
+
+	sc_list_init(&list);
+	sc_cb_init(&buf, 512);
+
+	cbot_get_members(event->bot, (char *)event->channel, &list);
+	cbot_send(event->bot, event->channel,
+	          "Members of channel %s (\"censoring\" to avoid ping)",
+	          event->channel);
+	sc_list_for_each_entry(ui, &list, list, struct cbot_user_info)
+	{
+		sc_cb_printf(&buf, "%c*%s ", ui->username[0], &ui->username[1]);
+		if (buf.length >= 500) {
+			cbot_send(event->bot, event->channel, buf.buf);
+			sc_cb_clear(&buf);
+		}
+	}
+	if (buf.length > 0) {
+		cbot_send(event->bot, event->channel, buf.buf);
+	}
+	sc_cb_destroy(&buf);
+}
+
+void who_load(struct cbot *bot)
+{
+	cbot_register(bot, CBOT_ADDRESSED, (cbot_handler_t)who, NULL,
+	              "[wW]ho\\??");
+}
