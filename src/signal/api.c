818d0c88d587524542ef2f5f2a9dbe85c31eb611
Stephen Brennan <stephen@brennan.io>
2022-02-17 20:40:04 -0800

    signal: sig_expect now waits for expected message

--- src/signal/api.c
+++ src/signal/api.c
@@ -199,21 +199,36 @@ void sig_expect(struct cbot_signal_backend *sig, const char *type)
 {
 	struct jmsg *jm;
 	size_t ix_type;
-	jm = jmsg_read_parse(sig);
-	if (!jm) {
-		fprintf(stderr, "sig_expect: error reading jmsg\n");
-		return;
+	bool found = false;
+
+	/*
+	 * TODO: this drops unrelated messages which is... dumb
+	 * Need to redesign JMSG reading so that we queue up JMSGs for
+	 * processing, then place all unrelated JMSGs on that queue here.
+	 */
+	while (!found) {
+		jm = jmsg_read_parse(sig);
+		if (!jm) {
+			fprintf(stderr, "sig_expect: error reading jmsg\n");
+			jmsg_free(jm);
+			return;
+		}
+		ix_type = json_object_get(jm->orig, jm->tok, 0, "type");
+		if (ix_type == 0) {
+			fprintf(stderr, "sig_expect: no \"type\" field found "
+			                "in jmsg\n");
+		} else if (!json_string_match(jm->orig, jm->tok, ix_type,
+		                              type)) {
+			fprintf(stderr,
+			        "sig_expect: expected message type %s, but got "
+			        "something else, waiting still...\n",
+			        type);
+		} else {
+			printf("sig_expect: got message type %s\n", type);
+			found = true;
+		}
+		jmsg_free(jm);
 	}
-	ix_type = json_object_get(jm->orig, jm->tok, 0, "type");
-	if (ix_type == 0)
-		fprintf(stderr,
-		        "sig_expect: no \"type\" field found in jmsg\n");
-	else if (!json_string_match(jm->orig, jm->tok, ix_type, type))
-		fprintf(stderr,
-		        "sig_expect: expected message type %s, but got "
-		        "something else\n",
-		        type);
-	jmsg_free(jm);
 }
 
 const static char fmt_send_group[] = ("\n{"
