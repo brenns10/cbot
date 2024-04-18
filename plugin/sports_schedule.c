73bd3583c3b25ac59ec997d51ca678b6b9cc511f
Stephen Brennan <stephen@brennan.io>
2024-04-18 00:55:19 -0700

    sports_schedule: add error flag
    When error flag is set, report it in the message so that I know I should
    check the debug logs.
    
    Signed-off-by: Stephen Brennan <stephen@brennan.io>

--- plugin/sports_schedule.c
+++ plugin/sports_schedule.c
@@ -33,7 +33,7 @@ const char *NBA_TEAM = "GSW";
 const char *NBA_TEAMNAME = "Golden State Warriors";
 
 static int search_nba_games(struct json_easy *je, int year, int month, int day,
-                            char **time)
+                            char **time, bool *err)
 {
 	uint32_t obj;
 	char date[12];
@@ -41,6 +41,7 @@ static int search_nba_games(struct json_easy *je, int year, int month, int day,
 
 	if (je_get_array(je, 0, "resultSets", &obj) != JSON_OK) {
 		CL_WARN("nba: no 'resultSets' key found");
+		*err = true;
 		return -1; /* not found */
 	}
 	CL_DEBUG("resultSets = %u\n", obj);
@@ -87,7 +88,7 @@ static int search_nba_games(struct json_easy *je, int year, int month, int day,
 }
 
 static int check_nba(struct cbot *bot, int year, int month, int day,
-                     char **time)
+                     char **time, bool *err)
 {
 	/* NBA season overlaps, use the earlier year. Latest the finals could be
 	 * is July */
@@ -103,9 +104,10 @@ static int check_nba(struct cbot *bot, int year, int month, int day,
 	int ret = json_easy_parse(&je);
 	if (ret != JSON_OK) {
 		CL_WARN("nba/json: error: %s\n", json_strerror(ret));
+		*err = true;
 		ret = -1;
 	} else {
-		ret = search_nba_games(&je, year, month, day, time);
+		ret = search_nba_games(&je, year, month, day, time, err);
 	}
 	json_easy_destroy(&je);
 	free(data);
@@ -120,7 +122,7 @@ const char *MLB_SCHED =
 const char *MLB_TEAM = "San Francisco Giants";
 
 static int search_mlb_games(struct json_easy *je, int year, int month, int day,
-                            char **time)
+                            char **time, bool *err)
 {
 	uint32_t dates;
 	char date[12];
@@ -131,6 +133,7 @@ static int search_mlb_games(struct json_easy *je, int year, int month, int day,
 	int ret = je_get_array(je, 0, "dates", &dates);
 	if (ret != JSON_OK) {
 		CL_WARN("mlb: 'dates' not found in result\n");
+		*err = true;
 		return -1;
 	}
 
@@ -139,11 +142,13 @@ static int search_mlb_games(struct json_easy *je, int year, int month, int day,
 	{
 		if (!je_string_match(je, obj, "date", date)) {
 			CL_DEBUG("mlb: skip non-matching date (%s)\n", date);
+			*err = true;
 			continue;
 		}
 		uint32_t games, game;
 		if (je_get_array(je, obj, "games", &games) != JSON_OK) {
 			CL_WARN("mlb: missing 'games' in date result\n");
+			*err = true;
 			continue;
 		}
 		je_arr_for_each(game, je, games)
@@ -155,6 +160,7 @@ static int search_mlb_games(struct json_easy *je, int year, int month, int day,
 			if (je_get_string(je, game, "gameDate", &timestamp) !=
 			    JSON_OK) {
 				CL_WARN("mlb: could not load 'gameDate'\n");
+				*err = true;
 				continue;
 			}
 			/*
@@ -173,6 +179,7 @@ static int search_mlb_games(struct json_easy *je, int year, int month, int day,
 			free(timestamp);
 			if (!ret || *ret) {
 				CL_WARN("mlb: failed to parse 'gameDate'\n");
+				*err = true;
 				continue;
 			}
 			time_t epoch = timegm(&broken);
@@ -182,9 +189,9 @@ static int search_mlb_games(struct json_easy *je, int year, int month, int day,
 				CL_WARN("mlb: failed to format game date: %s\n",
 				        strerror(errno));
 				free(timeval);
+				*err = true;
 				continue;
 			}
-			CL_INFO("mlb: timeval: %s\n", timeval);
 			*time = timeval;
 			return 1;
 		}
@@ -193,7 +200,7 @@ static int search_mlb_games(struct json_easy *je, int year, int month, int day,
 }
 
 static int check_mlb(struct cbot *bot, int year, int month, int day,
-                     char **time)
+                     char **time, bool *err)
 {
 	char *data = cbot_curl_get(bot, MLB_SCHED, year, month, day, year,
 	                           month, day);
@@ -205,9 +212,10 @@ static int check_mlb(struct cbot *bot, int year, int month, int day,
 	int ret = json_easy_parse(&je);
 	if (ret != JSON_OK) {
 		CL_WARN("mlb/json: error: %s\n", json_strerror(ret));
+		*err = true;
 		ret = -1;
 	} else {
-		ret = search_mlb_games(&je, year, month, day, time);
+		ret = search_mlb_games(&je, year, month, day, time, err);
 	}
 	json_easy_destroy(&je);
 	free(data);
@@ -218,7 +226,6 @@ struct arg {
 	char *channel;
 	struct cbot *bot;
 	struct tm tm;
-	bool report_empty;
 	bool reschedule;
 };
 
@@ -227,17 +234,18 @@ static void run_thread(void *varg)
 	struct arg *arg = varg;
 	char *mlb_time, *nba_time;
 	struct sc_charbuf msg;
+	bool err = false;
 
 	sc_cb_init(&msg, 256);
 	int nba = check_nba(arg->bot, arg->tm.tm_year, arg->tm.tm_mon,
-	                    arg->tm.tm_mday, &nba_time);
+	                    arg->tm.tm_mday, &nba_time, &err);
 	if (nba > 0) {
 		sc_cb_printf(&msg, "The %s have a home game at %s.",
 		             NBA_TEAMNAME, nba_time);
 		free(nba_time);
 	}
 	int mlb = check_mlb(arg->bot, arg->tm.tm_year, arg->tm.tm_mon,
-	                    arg->tm.tm_mday, &mlb_time);
+	                    arg->tm.tm_mday, &mlb_time, &err);
 	if (mlb > 0) {
 		if (msg.length)
 			sc_cb_concat(&msg, " Also, the ");
@@ -249,13 +257,18 @@ static void run_thread(void *varg)
 		sc_cb_printf(&msg, "%s have a home game at %s.", MLB_TEAM, s);
 		free(mlb_time);
 	}
-	if (msg.length)
-		cbot_send(arg->bot, arg->channel, "%s", msg.buf);
-	else if (arg->report_empty)
-		cbot_send(arg->bot, arg->channel,
-		          "No home games for %s or %s on %04d-%02d-%02d",
-		          NBA_TEAMNAME, MLB_TEAM, arg->tm.tm_year,
-		          arg->tm.tm_mon, arg->tm.tm_mday);
+	if (!msg.length)
+		sc_cb_printf(&msg,
+		             "No home games for %s or %s on %04d-%02d-%02d.",
+		             NBA_TEAMNAME, MLB_TEAM, arg->tm.tm_year,
+		             arg->tm.tm_mon, arg->tm.tm_mday);
+	if (err)
+		sc_cb_printf(&msg,
+		             " BTW: I encountered an error in my requests, so"
+		             " my result may be inaccurate. Check logs for "
+		             "details.");
+
+	cbot_send(arg->bot, arg->channel, "%s", msg.buf);
 	sc_cb_destroy(&msg);
 	free(arg->channel);
 	free(arg);
@@ -298,7 +311,6 @@ static void callback(struct cbot_plugin *plugin, void *arg)
 	struct arg *a = calloc(1, sizeof(*a));
 	a->channel = strdup(CHANNEL);
 	a->bot = bot;
-	a->report_empty = true;
 	a->reschedule = true;
 	localtime_r(&ts, &a->tm);
 	a->tm.tm_year += 1900;
@@ -332,7 +344,6 @@ static void handler(struct cbot_message_event *evt, void *user)
 	arg->channel = strdup(evt->channel);
 	arg->bot = evt->bot;
 	arg->tm = tm;
-	arg->report_empty = true;
 	arg->reschedule = false;
 	sc_lwt_create_task(cbot_get_lwt_ctx(evt->bot), run_thread, arg);
 }
