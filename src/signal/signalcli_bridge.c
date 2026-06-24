6b3fc9415c74159fc76179ad1cbb941399d4caea
Stephen Brennan <stephen@brennan.io>
2026-06-24 10:24:51 -0700

    signal-cli: fix broken updateProfile call
    json.dumps() never did this to me. Where did I go wrong?
    
    Signed-off-by: Stephen Brennan <stephen@brennan.io>

--- src/signal/signalcli_bridge.c
+++ src/signal/signalcli_bridge.c
@@ -98,7 +98,7 @@ static uint64_t signalcli_send_group(struct cbot_signal_backend *sig,
 static const char fmt_nick[] =
         ("{\"jsonrpc\":\"2.0\",\"method\":\"updateProfile\","
          "\"id\":\"%lu\",\"params\":{\"name\":\"%s\","
-         "\"aboutEmoji\":\"🤖\",\"about\",\"I'm a bot! "
+         "\"aboutEmoji\":\"🤖\",\"about\":\"I'm a bot! "
          "https://github.com/brenns10/cbot\"}}\n");
 
 static void signalcli_nick(const struct cbot *bot, const char *newnick)
